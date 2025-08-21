eb4010cce4f4dc7cdcc590fe6805c0f88d553ca7
MrZloHex <zlo.alex.it@gmail.com>
2025-08-21 06:42:16 +0300

    init

---
+++ cmd/achtung/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/lmittmann/tint"
+	log "log/slog"
+
+	cli "github.com/spf13/pflag"
+
+	"achtung/pkg/protocol"
+)
+
+var logLevelMap = map[string]log.Level{
+	"debug": log.LevelDebug,
+	"info":  log.LevelInfo,
+	"warn":  log.LevelWarn,
+	"error": log.LevelError,
+}
+
+func main() {
+	url := cli.StringP("url", "u", "ws://localhost:8092", "Url of hub")
+	logLevel := cli.StringP("log", "l", "info", "Log level")
+	cli.Parse()
+
+	log.SetDefault(log.New(tint.NewHandler(os.Stdout, &tint.Options{
+		Level: logLevelMap[*logLevel],
+	})))
+
+	ptcl_cfg := protocol.PtclConfig{
+		Shard:  "ACHTUNG",
+		Url:    *url,
+		Reconn: 5,
+	}
+
+	ptcl, err := protocol.NewProtocol(ptcl_cfg)
+	if err != nil {
+		log.Error("Failed to init protocol")
+		os.Exit(1)
+	}
+
+	log.Info("BOOTING UP", "url", ptcl_cfg.Url)
+
+	go ptcl.Run()
+
+	for {
+		cmd := ptcl.Receive()
+
+		log.Info("Got msg", "msg", string(cmd))
+	}
+}
